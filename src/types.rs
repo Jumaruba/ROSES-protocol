d41f892e71f4fa0ee9a108ac2c8d13696f7516e4
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2023-03-09 16:27:02 +0000

    Add display for some types

--- src/types.rs
+++ src/types.rs
@@ -10,16 +10,34 @@ pub struct Ck{
     pub dck: i64
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Clone, PartialEq, Eq, Hash)]
 pub struct TagElement<E: Eq + Clone + Hash + Debug + Display>{
     pub sck: i64,
     pub n: i64,
     pub elem: E
 }
 
+impl<E: Eq + Clone + Hash + Debug + Display> Display for TagElement<E>{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {}, {})", self.sck, self.n, self.elem)
+    }
+}
+
+
+impl<E: Eq + Clone + Hash + Debug + Display> Debug for TagElement<E>{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub struct Dot {
     pub id: NodeId,
     pub sck: i64,
     pub n: i64
+}
+
+impl Display for Dot {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{},{})", self.id, self.sck, self.n)
+    }
 }
